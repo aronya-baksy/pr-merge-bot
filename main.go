bbda4de1d31f96172f6f7e7f26085a44620d04b2
aronya-baksy <aronya.baksy@hpe.com>
2023-06-08 13:17:00 +0530

    Rough implementation of bot

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/swinton/go-probot/probot"
+)
+
+func main() {
+	probot.HandleEvent("pull-request", func(ctx *probot.Context) error {
+		event := ctx.Payload.(*github.PullRequestEvent)
+		pr := event.GetPullRequest()
+		repo := event.GetRepo()
+
+		log.Printf("Reviewing PR with title %v", pr.Number)
+
+		// Start PR review
+		prReview := &github.PullRequestReviewRequest{CommitID: pr.Head.SHA}
+
+		// Check PR title for value, and approve if present
+		if strings.Contains(*pr.Title, "Tenant Onboarding Request") {
+			prReview.Event = github.String("APPROVE")
+			prReview.Body = github.String("LGTM")
+		}
+
+		// Send approval to GitHub API
+		review, _, err := ctx.GitHub.PullRequests.CreateReview(context.Background(), *repo.Owner.Login, *repo.Name, pr.GetNumber(), prReview)
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Approved PR Review Request: %v", review)
+		return nil
+	})
+
+	probot.Start()
+}
